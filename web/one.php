730e6c008f98715244ad5c6ed8ba03f74293518f
Jeff Royer Admin <jlroyer_admin@ou.ad3.ucdavis.edu>
2018-12-27 23:36:39 -0800

    test

--- web/one.php
+++ web/one.php
@@ -1,4 +1,16 @@
 <?php
 	print('ONE.PHP<br/>');
-	phpInfo();
+	$db = parse_url(getenv("DATABASE_URL"));
+
+	$path = ltrim($db["path"],"/");
+	print("PATH:  " . $path);
+	print("<br/>");
+	print("USER:  " . $db["user"]);
+
+	print("<br/>");
+	print("HOST:  " . $db["host"]);
+
+	print("<br/>");
+	print("PORT:  " . $db["port"]);
+
 ?>

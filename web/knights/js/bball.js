bf1b534409b0da6acb30afdc82a9b4a69d8a9630
themountee1973 <royer.j@mac.com>
2019-01-16 14:54:14 -0800

    fixes

--- web/knights/js/bball.js
+++ web/knights/js/bball.js
@@ -8,6 +8,100 @@
 			'homeq1', 'homeq2', 'homeq3', 'homeq4', 'awayq1', 'awayq2',
 			'awayq3','awayq4','homescore','awayscore'];
 
+
+
+		function initTable() {
+				oTable = $('#statistics').dataTable({
+					bJQueryUI: true,
+					sPaginationType : 'full_numbers',
+					iDisplayLength: -1,
+					columns		:	[
+						{bVisible	:	false},
+						{'sClass':'name'},
+						{'sClass':'center'},
+						{'sClass':'aafga center split'},
+						{'sClass':'aafgm center split'},
+						{'sClass':'aatpa center split'},
+						{'sClass':'aatpm center split'},
+						{'sClass':'aafta center split'},
+						{'sClass':'aaftm center split'},
+						{'sClass':'aaorb center split'},
+						{'sClass':'aadrb center split'},
+						{'sClass':'aaast center'},
+						{'sClass':'aablk center'},
+						{'sClass':'aastl center'},
+						{'sClass':'aatip center'},
+						{'sClass':'aachg center'},
+						{'sClass':'aatos center'}
+					]
+				});
+		}
+		function initGameSelector() {
+			$('#game').change(function(){
+				var gameId = $(this).find('option:selected').val();
+				
+				if(gameId == '') {
+					gameId = 'all';
+				}
+
+				$.ajax({
+					dataType		:	'json',
+					type			:	'GET',
+					url				:	'json/stats-' + gameId + '.json',
+					success			:	function(json){
+						var a = [];
+						if( ('undefined' !== json) && (json.length >= 1) ) {
+							for(var i = 0; i < json.length; i++) {
+								var inner = [];
+								for(var j = 0; j < statElements.length; j++) {
+									inner.push(json[i][statElements[j]]);
+								}
+								a.push(inner);
+							}
+						}
+						oTable.fnClearTable(true);
+						oTable.fnAddData(a, true);
+						totals();
+					}
+				});
+
+				$.ajax({
+					dataType		:	'json',
+					type			:	'GET',
+					url				:	'json/games.json',
+					success			:	function(json){
+						for(var x = 0; x < json.length; x++) {
+							
+							if(json[x].id == gameId) {
+								updateScore(json[x]);
+							}
+						}
+					}
+				});
+
+			});
+		}
+
+		function fetchGames() {
+			var selectGame = 'Select game:  ';
+
+			$.ajax({
+				dataType		:	'json',
+				type			:	'GET',
+				url				:	'json/games.json',
+				success			:	function(json){
+									selectGame += '<select name="game" id="game">';
+									selectGame += '<option value=""></option>';
+									for(var a = 0; a < json.length; a++) {
+										selectGame += '<option value="' + json[a].id + '">' + json[a].away + ' @ ' + json[a].home + '</option>';											
+									}
+									selectGame += '</select>';
+									$('#game').empty().append(selectGame);
+				}
+			});
+
+		}
+
 		function totals() {
 			var fgaTtl = 0;
 			var fgmTtl = 0;
@@ -81,21 +175,58 @@
 			for(var x = 0; x < tos.length; x++) {
 				toTtl += parseInt($(tos[x]).text());
 			}
-			$('#fgattl').empty().append(fgaTtl);
-			$('#fgmttl').empty().append(fgmTtl);
-			$('#tpattl').empty().append(tpaTtl);
-			$('#tpmttl').empty().append(tpmTtl);
-			$('#ftattl').empty().append(ftaTtl);
-			$('#ftmttl').empty().append(ftmTtl);
-			$('#orebttl').empty().append(orebTtl);
-			$('#drebttl').empty().append(drebTtl);
-			$('#asstttl').empty().append(astTtl);
-			$('#blkttl').empty().append(blkTtl);
-			$('#stlttl').empty().append(stlTtl);
-			$('#tipsttl').empty().append(pdTtl);
-			$('#chrgttl').empty().append(ctTtl);
-			$('#tosttl').empty().append(toTtl);
+			$('.fgattl').empty().append(fgaTtl);
+			$('.fgmttl').empty().append(fgmTtl);
+			$('.fgpct').empty().append( ((fgmTtl/fgaTtl).toFixed(3)));
 
+			$('.tpattl').empty().append(tpaTtl);
+			$('.tpmttl').empty().append(tpmTtl);
+			$('.tppct').empty().append( ((tpmTtl/tpaTtl).toFixed(3)));
+
+			$('.ftattl').empty().append(ftaTtl);
+			$('.ftmttl').empty().append(ftmTtl);
+			$('.ftpct').empty().append(  ((ftmTtl/ftaTtl).toFixed(3)));	
+
+			$('.orebttl').empty().append(orebTtl);
+			$('.drebttl').empty().append(drebTtl);
+			$('.rebttl').empty().append(orebTtl + drebTtl);
+
+			$('.asstttl').empty().append(astTtl);
+			$('.blkttl').empty().append(blkTtl);
+			$('.stlttl').empty().append(stlTtl);
+			$('.tipsttl').empty().append(pdTtl);
+			$('.chrgttl').empty().append(ctTtl);
+			$('.tosttl').empty().append(toTtl);
+
+		}
+
+		function updateScore(json) {
+			
+				var ht = json.home;
+				var vt = json.away;
+				var homeScore = json.homescore;
+				var awayScore = json.awayscore;
+				var v1 = json.awayq1;
+				var v2 = json.awayq2;
+				var v3 = json.awayq3;
+				var v4 = json.awayq4;
+				var h1 = json.homeq1;
+				var h2 = json.homeq2;
+				var h3 = json.homeq3;
+				var h4 = json.homeq4;
+				var dt = json.day;
+				$('#awayTeam').empty().append(vt);
+				$('#homeTeam').empty().append(ht);
+				$('#awayQ1').empty().append(v1);
+				$('#awayQ2').empty().append(v2);
+				$('#awayQ3').empty().append(v3);
+				$('#awayQ4').empty().append(v4);
+				$('#homeQ1').empty().append(h1);
+				$('#homeQ2').empty().append(h2);
+				$('#homeQ3').empty().append(h3);
+				$('#homeQ4').empty().append(h4);
+				$('#awayTtl').empty().append(awayScore);
+				$('#homeTtl').empty().append(homeScore);
 		}
 
 //})(jQuery);

82443401f89c213f1a8061d8d95c448c12447f5e
themountee1973 <royer.j@mac.com>
2019-06-18 14:28:08 -0700

    more testing

---
+++ web/wsd/services/teams.php
@@ -0,0 +1,34 @@
+<?php
+
+	header('Content-type: application/json');
+
+	include_once './in.php';
+
+	$response = array();
+	$records = array();
+
+	$query;
+
+	$param = $_GET['conf'];
+
+	if( !isset($param) ) {
+	
+		$query = 'SELECT * FROM getTeams();';
+		
+	} else {
+		
+		$query = 'SELECT * FROM getTeamsByConference(\'' . $param . '\');';
+
+	}
+
+	$resultSet = $pdo->query($query);
+	
+	while($row = $resultSet->fetch(PDO::FETCH_BOTH)) {
+		array_push($records, $row);
+	}
+
+	$response['success'] = 1;
+	$response['records'] = $records;
+
+	echo json_encode($response);
+?>

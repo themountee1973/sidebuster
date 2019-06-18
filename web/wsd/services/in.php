e5f8eafb3b8d2bb85e40fd5ca41b323471137f41
themountee1973 <royer.j@mac.com>
2019-06-18 14:14:37 -0700

    more testing

--- web/wsd/services/in.php
+++ web/wsd/services/in.php
@@ -1,15 +1,11 @@
 <?php
 	
 	$db = parse_url(getenv("DATABASE_URL"));
-print_r('Testing....' . $db);
-	$pdoDsn = 'host=ec2-54-235-178-189.compute-1.amazonaws.com;dbname=d2tjlsr5ea65g5';
 
-	$pdo = new PDO("pgsql:" . sprintf(
-		$db["host"], 
-		$db["port"], 
-		$db["user"], 
-		$db["pass"], 
-		ltrim($db["path"], "/")
-	));
-
+	try {
+		$pdo = new PDO("pgsql:host=" . $db["host"] . ';dbname=' . ltrim($db["path"], "/"), $db["user"], $db["pass"]);
+	} catch (Exception $e) {
+		print_r('Exception:  ' . $e);
+	}
+	
 ?>

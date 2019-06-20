0160291203dde23ce70930b56936a63acc9e3ce3
themountee1973 <royer.j@mac.com>
2019-06-20 01:39:56 -0700

    test

--- web/wsd/js/summary.js
+++ web/wsd/js/summary.js
@@ -30,10 +30,33 @@
 	var totalPointsAgainst = json.totalagainst;
  }
  
- function makeSummaryTable(list) {
+ function makeSummaryRow(list) {
 		
-	var tbl = 
-	 
+	var tbl = '<tr>';
+	
+	tbl += '<td class="conference">' + list.conference + '</td>';
+	tbl += '<td class="team">' + list.team + '</td>';
+	tbl += '<td class="wins">' + list.wins + '</td>';
+	tbl += '<td class="losses">' + list.losses + '</td>';
+	tbl += '<td class="conf-wins">' + list.cwins + '</td>';
+	tbl += '<td class="conf-losses">' + list.closses + '</td>';
+	tbl += '<td class="conf-pts-home">' + list.chomefor + '</td>';
+	tbl += '<td class="conf-pts-home-against">' + list.chomeagainst + '</td>';
+	tbl += '<td class="conf-pts-away">' + list.cawayfor + '</td>';
+	tbl += '<td class="conf-pts-away-against">' + list.cawayagainst + '</td>';
+	tbl += '<td class="conf-total-pts">' + list.cttlfor + '</td>';
+	tbl += '<td class="conf-total-pts-against">' + list.cttlagainst + '</td>';
+	tbl += '<td class="home-total-pts">' + list.homefor + '</td>';
+	tbl += '<td class="home-total-pts-against">' + list.homeagainst + '</td>';
+	tbl += '<td class="away-total-pts">' + list.awayfor + '</td>';
+	tbl += '<td class="away-total-pts-against">' + list.awayagainst + '</td>';
+	tbl += '<td class="total-pts">' + list.totalfor + '</td>';
+	tbl += '<td class="total-pts-against">' + list.totalagainst + '</td>';
+	
+	
+	tbl += '</tr>';
+	
+	return tbl;
  }
  
  function initSummary() {

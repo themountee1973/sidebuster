9205881bd61daed05def1d33f32331b9cdd9b367
themountee1973 <royer.j@mac.com>
2019-06-25 14:56:18 -0700

    more new

--- web/wsd/services/save.php
+++ web/wsd/services/save.php
@@ -22,12 +22,14 @@
 		$queryCreateRecord = 'SELECT createNewMeetRecord(\'' . $day . '\',' . $team1 . ',' . $team2 . ') as ID;';
 
 		print_r($queryCreateRecord);
-			print_r('1:  ' . $pdo);
-	
+
+		print_r('$recId = ' . $recId);
+
+		print_r('end test');
 		if ( !isset($_POST['hiddenID']) ) {
 
 			$idRes = $pdo->query($queryCreateRecord);
-			print_r('2:  ' . $pdo);
+
 			while( $row = $idRes->fetch(PDO::FETCH_BOTH) ) {
 				$recId = $row['ID'];
 			}
